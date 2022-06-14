fee87de82b82f4d46e6de789d156f085c8ca66f9
shanaka.dba@gmail.com <shanaka.dba@gmail.com>
2022-06-14 20:13:54 +0530

    final commited

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -1,11 +1,8 @@
-
 import { doc, onSnapshot, updateDoc } from 'firebase/firestore'
 import React ,{ useState, useEffect } from 'react'
-import {MdChevronLeft, MdChevronRight} from'react-icons/md'
 import { UserAuth } from '../context/AuthContext'
 import { db } from '../firebase'
 import {AiOutlineClose} from 'react-icons/ai'
-import { async } from '@firebase/util'
 
 const SavedShows = () => {
     const {user} = UserAuth()
@@ -16,15 +13,6 @@ const SavedShows = () => {
           setMovies(doc.data()?.savedShows);
         });
       }, [user?.email]);
-
-    const slideLeft =()=>{
-        var slider =document.getElementById('slider')
-        slider.scrollLeft=slider.scrollLeft - 500;
-    }
-    const slideRight =()=>{
-        var slider =document.getElementById('slider')
-        slider.scrollLeft=slider.scrollLeft + 500;
-    }
 
     const movieRef = doc(db, 'users', `${user?.email}`)
     const deleteShow=async(id)=>{
@@ -40,21 +28,17 @@ const SavedShows = () => {
 
   return (
     <>
-      <h2 className='text-white font-bold md:text-xl p-4'>My Shows</h2>
-        <div className='relative flex items-center group'>
-            <MdChevronLeft size={40} className='left-0 bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' onClick={slideLeft}/>
-            <div id={'slider'} className='w-full h-full overflow-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
-                {movies.map((item, id)=>(
-                    <div key={id} className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
+      <h2 className='text-white ml-2 md:ml-7 font-bold md:text-xl p-4'>Favourites</h2>
+        <div className='grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4 mx-5 md:mx-10 lg:mx-20'>
+            {movies.map((item, id)=>(
+                <div key={id} className='cursor-pointer relative p-1'>
                     <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.img}`} alt={item?.title} />
                     <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
                         <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
                         <p onClick={()=>deleteShow(item.id)} className='absolute text-gray-300 top-4 right-4'><AiOutlineClose /></p>
                     </div>
                 </div>
-                ))}
-            </div>
-            <MdChevronRight size={40} className='right-0 bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' onClick={slideRight}/>
+            ))}
         </div>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,12 @@ const Home = () => {
   return (
     <div>
       <Main />
-      <Row rowID='1' title='Up Coming' fetchURL={requests.requestPopular} />
+      <Row rowID='1' title='Popular' fetchURL={requests.requestPopular} />
+      <Row rowID='2' title='Up Coming' fetchURL={requests.requestUpcoming} />
+      <Row rowID='3' title='Top Rated' fetchURL={requests.requestTopRated} />
+      <Row rowID='4' title='Trending' fetchURL={requests.requestTrending} />
+      <Row rowID='5' title='Horror' fetchURL={requests.requestHorror} />
       
-      <Row rowID='3' title='Up Coming' fetchURL={requests.requestPopular} />
-      <Row rowID='4' title='Up Coming' fetchURL={requests.requestPopular} />
-      <Row rowID='5' title='Up Coming' fetchURL={requests.requestPopular} />
     </div>
   )
 }
