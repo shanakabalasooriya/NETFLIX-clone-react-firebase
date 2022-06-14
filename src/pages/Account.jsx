7c4e86d8abb87d60c6654b031c7b3eff29b49181
shanaka.dba@gmail.com <shanaka.dba@gmail.com>
2022-06-14 20:26:42 +0530

    finall commited

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -8,7 +8,7 @@ const Account = () => {
         <img className='w-full h-[400px] object-cover' src='https://www.logitheque.com/en/wp-content/uploads/sites/6/2019/07/netflix-image.jpg' alt='/' />
         <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'></div>
         <div className='absolute top-[20%] md:p-8'>
-          <h1 className='text-3xl mt-40 ml-4 md:text-5xl font-bold'>My Shows</h1>
+          <h1 className='text-3xl ml-4 md:text-4xl font-bold'>My Shows</h1>
         </div>
       </div>
     <SavedShows />
