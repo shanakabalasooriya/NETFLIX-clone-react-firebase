dd276e4c39a5ba80ee811444a038da45b9c34281
shanaka.dba@gmail.com <shanaka.dba@gmail.com>
2022-06-14 14:00:39 +0530

    3rdcommit

--- src/Requests.js
+++ src/Requests.js
@@ -1,7 +1,8 @@
 const key= process.env.REACT_APP_KEY;
 
 const requests = {
-    requestPopular:`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page1`,
+    requestPopular:`https://api.themoviedb.org/3/movie/rated?api_key=${key}&language=en-US&page1`,
+    requestRated:`https://api.themoviedb.org/3/movie/rated?api_key=${key}&language=en-US&page1`,
 }
 
 export default requests;
